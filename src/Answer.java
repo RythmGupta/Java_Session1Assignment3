d52c93d491021e88130d02d6539f95c5a93bfa24
RythmGupta <sunrit.rythm@gmail.com>
2016-08-14 07:17:19 -0700

    Java_Session1Assignment3

---
+++ src/Answer.java
@@ -0,0 +1,16 @@
+
+public class Answer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c='R';
+		int a=(int)c;
+		System.out.println(a);
+         
+	}
+
+}
+
+
+//Answer :String and char value cannot be stored in int variable
+// Double and float can be stored in int using type casting
